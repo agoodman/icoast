6c6a0a1982bee3e8c74033033855a4c0a44ef4dc
Aubrey Goodman <aubrey.goodman@gmail.com>
2013-05-10 00:46:06 -0400

    only assign default regime if blank

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,7 +1,7 @@
 class Tag < ActiveRecord::Base
   has_many :annotations
   has_many :images, through: :annotations
-  attr_accessible :name
+  attr_accessible :name, :regime
   
   scope :regime, where(regime: true)
   scope :visual, where('regime != ?',true)
@@ -9,7 +9,7 @@ class Tag < ActiveRecord::Base
   before_save :init_regime
   
   def init_regime
-    self.regime = false    
+    self.regime = false if regime.blank?
   end
   
 end
